335954b63119949869848c6247d1ba4743c68b3e
claudio <claudiocoppola90@gmail.com>
2016-06-07 17:43:42 +0100

    QTC3D HMM class

--- qsr_prob_rep/src/qsrrep_hmms/qtc3d_hmm.py
+++ qsr_prob_rep/src/qsrrep_hmms/qtc3d_hmm.py
@@ -7,6 +7,7 @@ Created on Thu May 26 15:34:51 2016
 from qsrrep_hmms.qtc_hmm_abstractclass import QTCHMMAbstractclass
 from qtc3d_converter.point_to_QTC3D import ind2qtc3d,qtc3d2ind
 import pickle
+import ghmm as gh
 import json
 import numpy as np
 
@@ -16,27 +17,135 @@ class QTC3DHMM(QTCHMMAbstractclass):
     
     def __init__(self):
         super(QTC3DHMM, self).__init__()
+        self.num_possible_states = 0
         
                 
     def _save_to_file(self,filename):
         with open(filename,'w') as f:
                 pickle.dump(self.f)
                 f.close()
-    def _create_transition_matrix(self, size, **kwargs):
+                
+    def _create_emission_matrix(self, size, **kwargs):
         """Creates a Conditional Neighbourhood Diagram for QTCB as a basis for the HMM.
 
         :param kwargs:
+            * size: unused
             * input_to_state: dict mapping the input symbol to the state of the hmm. Different for all 3 qtc versions.
 
         :return: The transition matrix only allowing transitions according to the CND
         """
         input_to_state = dict(kwargs["input_to_state"])
+        sequences_of_activity = kwargs["qsr_seq"]
         num_states=max(input_to_state.values())
-        tran=np.zeros((num_states,num_states))
+        num_emiss=3**5+2*(3**2)-1
+        emiss=np.ones((num_states+2,num_emiss))
+        for sequence in sequences_of_activity:
+            for sample in sequence:
+                emiss[input_to_state[sample],sample]+=1
+        return emiss/emiss.sum(axis=1).reshape(-1, 1)
+        
+        
+    def _create_transition_matrix(self, size, **kwargs):
+        """Creates a Conditional Neighbourhood Diagram for QTCB as a basis for the HMM.
 
+        :param kwargs:
+            * size: unused
+            * input_to_state: dict mapping the input symbol to the state of the hmm. Different for all 3 qtc versions.
+            * sequences_of_activity: the sequences of QSRs. This should be a list of state chains, i.e. a list of lists
+            
+        :return: The transition matrix only allowing transitions according to the CND
+        """
+        input_to_state = dict(kwargs["input_to_state"])
+        sequences_of_activity = kwargs["qsr_seq"]
+        
+        
+        
+        num_states=max(input_to_state.values())
+        tran=np.ones((num_states+2,num_states+2))
+        
+        for sequence in sequences_of_activity:
+            tran[0,input_to_state[sequence[0]]]+=1
+            tran[input_to_state[sequence[-1]],-1]+=1
+            for ind,sample in enumerate(sequence[:-1]):
+                tran[input_to_state[sample],input_to_state[sequence[ind+1]]]+=1
+        
+        
+        
+        tran[tran != 1] = 0
+        tran[tran == 0] = 0.00001
+        tran[0] = 1
+        tran[:, 0] = 0
+        tran[:, -1] = 1
+        tran[0, -1] = 0
+        tran[-1] = 0
+        tran += np.dot(np.eye(len(tran)), 0.00001)
+        tran[0, 0] = 0    
+        
+        tran=tran/tran.sum(axis=1).reshape(-1, 1)
         # Calling parent to generate actual matrix
-        return super(QTC3DHMM, self)._create_transition_matrix(size=size, qtc=qtc)
+        return tran
+    def _create(self, **kwargs):
+        """Creates and trains (using '_train') a HMM to represent the given qtc sequences.
+        Main function to create and train the hmm. Please override with special
+        behaviour if necessary.
 
+        This function is called by the library to create the hmm.
+
+        :param **kwargs:
+            * qsr_seq: the sequences of QSRs. This should be a list of state chains, i.e. a list of lists
+            * input_to_state: dict mapping the input symbol to the state of the hmm. Different for all 3 qtc versions.
+
+        :return: The trained HMM
+
+        """
+        input_to_state = dict(kwargs["input_to_state"])
+        state_seq = kwargs["qsr_seq"]
+        
+        num_states=max(input_to_state.values())
+        #state_seq = self._qsr_to_symbol(kwargs["qsr_seq"])
+        trans = self._create_transition_matrix(size=0, **kwargs)
+        emi = self._create_emission_matrix(size=0, **kwargs)
+        print len(trans),num_states+2
+        hmm = self._train(state_seq, trans, emi, num_states+2)
+        print '...done'
+        return hmm        
+        
+
+
+    def _train(self, seq, trans, emi, num_possible_states):
+        """Uses the given parameters to train a multinominal HMM to represent
+        the given seqences of observations. Uses Baum-Welch training.
+        Please override if special training is necessary for your QSR.
+
+        :param seq: the sequence of observations represented by alphabet symbols
+        :param trans: the transition matrix as a numpy array
+        :param emi: the emission matrix as a numpy array
+        :param num_possible_states: the total number of possible states
+
+        :return: the via baum-welch training generated hmm
+        """
+
+        print 'Generating HMM:'
+        print '\tCreating symbols...'
+        symbols = self._generate_alphabet(3**5+2*3**2-1)
+        startprob = np.zeros(num_possible_states)
+        startprob[0] = 1
+        print '\t\t', symbols
+        print '\tCreating HMM...'
+        print emi[:,0]
+        hmm = gh.HMMFromMatrices(
+            symbols,
+            gh.DiscreteDistribution(symbols),
+            trans.tolist(),
+            emi.tolist(),
+            startprob.tolist()
+        )
+        print '\tTraining...'
+        hmm.baumWelch(self._create_sequence_set(seq, symbols))
+
+        return hmm
+        
+        
     def _symbol_to_qsr(self, symbols):
         """Transforming alphabet symbols to QTCB states.
 
@@ -54,7 +163,7 @@ class QTC3DHMM(QTCHMMAbstractclass):
         return ret
 
     def symbol_to_qsr(self, symbol):
-        print "TODO"
+        
 
         return ind2qtc3d(symbol)
         
@@ -83,6 +192,6 @@ class QTC3DHMM(QTCHMMAbstractclass):
     
     
 if __name__ == '__main__':
-    a=QTC3DHMM()
-    print a
+    HMM=QTC3DHMM()
+    print HMM
     
